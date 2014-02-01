709e73da51b2e41afce60fc2e9cbfe89f5f52fbd
Karl Sutt <karl@sutt.ee>
2014-02-01 15:42:14 +0000

    Deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gocardless-clj "0.1.0-SNAPSHOT"
+(defproject gocardless-clj "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -6,4 +6,6 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [midje "1.6.0"]
                  [pandect "0.3.0"]
-                 [clj-http "0.7.8"]])
+                 [clj-http "0.7.8"]
+                 [org.clojure/data.json "0.2.4"]
+                 [clj-time "0.6.0"]])
