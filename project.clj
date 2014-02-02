689942e818ac8c1b607e20c7df3a24978e58ccf8
Karl Sutt <karl@sutt.ee>
2014-02-02 19:31:57 +0000

    Add midje and clj-http as dev dependencies

--- project.clj
+++ project.clj
@@ -3,8 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :profiles {:dev {:dependencies [[midje "1.6.0"]
+                                  [clj-http-fake "0.7.8"]]}}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [midje "1.6.0"]
                  [pandect "0.3.0"]
                  [clj-http "0.7.8"]
                  [org.clojure/data.json "0.2.4"]
