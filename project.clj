af2c79b6d920eab07609377519493b072a1bb782
Karl Sutt <karl@sutt.ee>
2014-02-03 19:23:06 +0000

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject gocardless-clj "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "Clojure client library for the GoCardless API"
+  :url "https://github.com/karls/gocardless-clj"
+  :license {:name "MIT License"
+            :url "http://choosealicense.com/licenses/mit/"}
   :profiles {:dev {:dependencies [[midje "1.6.0"]
                                   [clj-http-fake "0.7.8"]]}}
   :dependencies [[org.clojure/clojure "1.5.1"]
